df2d5836229b2daa5e7032e0a9206e2b971cf000
taeraemon <dbsl0512@naver.com>
2025-01-10 23:53:31 +0900

    Update sequence receive & handling logic

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,40 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
+String buf_serial = "";
+uint32_t prev_ms_log = 0;
+
 float tc_raw2phy(int adcValue) {
     float voltage = (adcValue / 4096.0) * 3.3;
     return (voltage - 1.25) / 0.005;    // (5mV/°C)
+}
+
+void processSerialInput(String input) {
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, input);
+
+    if (!error)
+    {
+        if (doc["seq"].is<int>()) {
+            int cmdCali = doc["cmd_cali"];
+            if (cmdCali) {  // Sequence Start
+                Serial.println("Sequence Start.");
+            }
+            else {          // Sequence Stop
+                Serial.println("Sequence Stop.");
+            }
+        }
+        else {
+            Serial.println("Unknown Command");
+        }
+    }
+    else {
+        Serial.print("Invalid JSON.");
+        Serial.print(error.f_str());
+        Serial.println();
+    }
+
+    buf_serial = "";
 }
 
 void setup()
@@ -31,6 +62,16 @@ void setup()
 
 void loop()
 {
+    while (Serial.available()) {
+        char incomingChar = Serial.read();
+        if (incomingChar == '\n') {
+            processSerialInput(buf_serial);
+        }
+        else {
+            buf_serial += incomingChar;
+        }
+    }
+
     JsonDocument doc;
 
     int adc1 = analogRead(ADC_TC_CH1);
